f676274fdf9a1d501393f36b77653b3dc2f601ea
leonardoolia <leonardooliaa@gmail.com>
2024-01-10 17:06:41 +0100

    created function to create cells

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ const rows = 10;
 const cols = 10;
 const cells = rows * cols;
 
+//? FUNZIONI
+
+const createCell = (content) => {
+
+    // Creo una cella
+    const newCell = document.createElement('div');
+    newCell.className = 'cell';
+    newCell.innerText = content;
+    return newCell;
+}
+
