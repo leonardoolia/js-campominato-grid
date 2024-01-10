42377a6b6bd00ee35ed2b169f261e8144f2afc10
leonardoolia <leonardooliaa@gmail.com>
2024-01-10 16:26:34 +0100

    got dom elements

---
+++ script.js
@@ -0,0 +1,9 @@
+//? RECUPERO GLI ELEMENTI DAL DOM
+const form = document.getElementById('form');
+const grid = document.getElementById('grid');
+
+// Dati di partenza
+const rows = 10;
+const cols = 10;
+const cells = rows * cols;
+
